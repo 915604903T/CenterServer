d3cd710d68c899773f3b88035f08a41290025fe3
tsm <18188116781@163.com>
2023-02-08 10:42:46 -0500

    finish debug user file send

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/915604903T/CenterServer/handlers"
 	"github.com/915604903T/CenterServer/helper"
 )
@@ -11,6 +13,6 @@ func main() {
 		ModelControllerHandler:      handlers.MakeModelControllerHandler(),
 		RelocaliseControllerHandler: handlers.MakeRelocaliseControllerHandler(),
 	}
-
+	log.Print("Center server start at port: 23333")
 	helper.Serve(&centerHandlers)
 }
